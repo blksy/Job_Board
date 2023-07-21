e8833067225b4f20b9b6b5ee8e50fb9ca061b2bb
blksy <79380937+blksy@users.noreply.github.com>
2023-07-21 13:41:48 +0200

    created the initial navigation menu with the MainNav component, also created a Header component that partially fills the background

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
-  <div></div>
+  <MainNav />
+  <Header />
 </template>
+
+<script>
+import MainNav from '@/components/MainNav.vue'
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainNav,
+    Header
+  }
+}
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+  font-family: 'Nunito', sans-serif;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <h1 class="header__title">
+      FIND YOUR <br /><span>NEW PERFECT</span><br />
+      JOB TODAY
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 500px;
+  background-image: url('../assets/job.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  position: relative;
+}
+
+.header__title {
+  position: absolute;
+  top: 7rem;
+  left: 3rem;
+  letter-spacing: 3;
+  font-weight: bolder;
+  font-size: 50px;
+  color: white;
+  text-shadow: 4px 2px 0px rgba(0, 0, 0, 0.2);
+}
+.header__title > span {
+  color: chartreuse;
+}
+</style>
