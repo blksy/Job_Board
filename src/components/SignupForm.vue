4af2f694d2ca5085013df42ac3ece73fbddf5a20
blksy <79380937+blksy@users.noreply.github.com>
2023-08-07 18:56:17 +0200

    created a composable for registering new users, user can now register his account which will be added to firebase db

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,16 +5,9 @@
       type="text"
       required
       placeholder="Name and surname"
-      v-model="displayFullName"
+      v-model="displayName"
     />
     <input class="inputFields" type="email" required placeholder="email" v-model="email" />
-    <input
-      class="inputFields"
-      type="tel"
-      required
-      placeholder="phone number Format: 123-456-789"
-      pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}"
-    />
     <input class="inputFields" type="password" required placeholder="password" v-model="password" />
     <button class="button">Sign up</button>
   </form>
@@ -22,19 +15,25 @@
 
 <script>
 import { ref } from 'vue'
+import useSignup from '../composables/useSignup.js'
+import { useRouter } from 'vue-router'
 
 export default {
   setup() {
-    const displayFullName = ref('')
+    const { error, signup } = useSignup()
+    //references
+    const router = useRouter()
+    const displayName = ref('')
     const email = ref('')
     const phone = ref('')
     const password = ref('')
 
-    const handleSubmit = () => {
-      console.log(displayFullName.value, email.value, phone.value, password.value)
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value)
+      router.push('/profile')
     }
 
-    return { displayFullName, email, phone, password, handleSubmit }
+    return { displayName, email, phone, password, handleSubmit }
   }
 }
 </script>
